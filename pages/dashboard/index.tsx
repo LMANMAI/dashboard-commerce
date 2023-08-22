125874a44685469ff97528bb3b7fa7b41981f6d8
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2023-08-22 18:45:17 -0300

    iconos y menu

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,52 +2,50 @@ import React from "react";
 import { Outlet, NavLink } from "react-router-dom";
 import type, { Menu, MenuProps } from "antd";
 import {
-  AppstoreOutlined,
-  MailOutlined,
-  SettingOutlined,
+  HomeOutlined,
+  PlusCircleOutlined,
+  FolderOpenOutlined,
+  RiseOutlined,
 } from "@ant-design/icons";
-
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: "group"
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  } as MenuItem;
-}
-
-import { SideMenu, MenuItem } from "./styles";
+import Icon from "@ant-design/icons";
+import { SideMenu, MenuContainer } from "./styles";
 const DashboardPage = () => {
-  const items: MenuProps["items"] = [
-    getItem("Navigation One", "sub1", <MailOutlined />, [getItem("Option 1", "1"), getItem("Option 2", "2"),getItem("Option 3", "3"), getItem("Option 4", "4")]), ];
-  const onClick: MenuProps["onClick"] = (e) => {
-    console.log("click ", e);
-  };
+  const menu = [
+    { icon: HomeOutlined, label: "Inicio", path: "" },
+    { icon: PlusCircleOutlined, label: "Agregar", path: "add" },
+    { icon: FolderOpenOutlined, label: "Mis productos", path: "products" },
+    { icon: RiseOutlined, label: "Mis ventas", path: "sales" },
+  ];
+
   return (
-    <div style={{display: 'flex', height: '100vh'}}>
+    <div style={{ display: "flex", height: "100vh" }}>
       <SideMenu>
-        
-        <Menu
+        {/* <Menu
           onClick={onClick}
           style={{ width: 256 }}
           defaultSelectedKeys={["1"]}
           defaultOpenKeys={["sub1"]}
           mode="inline"
           items={items}
-        />
+        /> */}
+        <MenuContainer>
+          {menu.map((item) => {
+            return (
+              <NavLink to={item.path}>
+                <Icon
+                  type="message"
+                  component={item.icon}
+                  style={{ fontSize: "16px", color: "#08c" }}
+                />
+                <p>{item.label}</p>
+              </NavLink>
+            );
+          })}
+        </MenuContainer>
       </SideMenu>
-     <div style={{width: '100%'}}>
-     <Outlet />
-     </div>
+      <div style={{ width: "100%" }}>
+        <Outlet />
+      </div>
     </div>
   );
 };
--- pages/dashboard/styles.tsx
+++ pages/dashboard/styles.tsx
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
-export const SideMenu = styled.div``;
+export const SideMenu = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  min-width: 100px;
+  display: flex;
+  align-items: center;
+`;
+export const MenuContainer = styled.ul`
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+`;
 export const MenuItem = styled.li`
   a {
     text-decoration: none;
