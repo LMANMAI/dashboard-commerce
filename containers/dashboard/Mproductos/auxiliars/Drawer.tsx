684923b0a795e97323b23609c9cc9d0d3efae2cb
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2023-11-08 12:00:11 -0300

    Update Drawer.tsx

--- containers/dashboard/Mproductos/auxiliars/Drawer.tsx
+++ containers/dashboard/Mproductos/auxiliars/Drawer.tsx
@@ -23,7 +23,7 @@ const Drawer = ({
   getData,
   onClose,
 }: any) => {
-  const [imgs1, setImgsList1] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const handleUpdateProduct = async (item: any) => {
     const res = await editProduct(item);
     if (res.status === 200) {
@@ -48,8 +48,12 @@ const Drawer = ({
       authorization: "authorization-text",
     },
     onChange(info) {
-      if (info.file.status === "uploading") {
-        setImgsList1(info.fileList);
+      if (info.file.status === "done") {
+        setLoading(true);
+
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
       } else if (info.file.status === "error") {
       }
     },
@@ -61,43 +65,28 @@ const Drawer = ({
         <p>Editar producto</p>
         <Switch size="small" onChange={onChange} />
       </EditMode>
-      <DetailImgContainer>
-        <DetailImgPosterPath>
-          {selectedItem && (
-            <img style={{ width: "100%" }} src={selectedItemPoster} />
-          )}
-        </DetailImgPosterPath>
+      {loading ? (
+        "cargando"
+      ) : (
+        <div>
+          <DetailImgContainer>
+            <DetailImgPosterPath>
+              {selectedItem && (
+                <img style={{ width: "100%" }} src={selectedItemPoster} />
+              )}
+            </DetailImgPosterPath>
 
-        <DetailImgDetail>
-          {selectedItem && (
-            <div
-              className="img_detail"
-              onClick={() =>
-                setSelectedItemPoster(
-                  `https://res.cloudinary.com/${
-                    import.meta.env.VITE_CLOUD_NAME
-                  }/image/upload/v1697492964/${selectedItem.posterPathImage}`
-                )
-              }
-            >
-              <img
-                style={{ width: "100%" }}
-                src={`https://res.cloudinary.com/${
-                  import.meta.env.VITE_CLOUD_NAME
-                }/image/upload/v1697492964/${selectedItem.posterPathImage}`}
-              />
-            </div>
-          )}
-          {selectedItem && selectedItem.imgs.length > 0
-            ? selectedItem.imgs.map((item: any, index: any) => (
+            <DetailImgDetail>
+              {selectedItem && (
                 <div
-                  key={index}
                   className="img_detail"
                   onClick={() =>
                     setSelectedItemPoster(
                       `https://res.cloudinary.com/${
                         import.meta.env.VITE_CLOUD_NAME
-                      }/image/upload/v1697492964/${item}`
+                      }/image/upload/v1697492964/${
+                        selectedItem.posterPathImage
+                      }`
                     )
                   }
                 >
@@ -105,126 +94,150 @@ const Drawer = ({
                     style={{ width: "100%" }}
                     src={`https://res.cloudinary.com/${
                       import.meta.env.VITE_CLOUD_NAME
-                    }/image/upload/v1697492964/${item}`}
+                    }/image/upload/v1697492964/${selectedItem.posterPathImage}`}
                   />
                 </div>
-              ))
-            : null}
-          <StyledUpload
-            style={{ width: "50px" }}
-            listType="picture-card"
-            multiple
-            className="upload-list-inline"
-            {...props1}
-          >
-            <div>
-              <PlusOutlined />
-              <div style={{ marginTop: 8 }}>Subir</div>
-            </div>
-          </StyledUpload>
-        </DetailImgDetail>
-      </DetailImgContainer>
+              )}
+              {selectedItem && selectedItem.imgs.length > 0
+                ? selectedItem.imgs.map((item: any, index: any) => (
+                    <div
+                      key={index}
+                      className="img_detail"
+                      onClick={() =>
+                        setSelectedItemPoster(
+                          `https://res.cloudinary.com/${
+                            import.meta.env.VITE_CLOUD_NAME
+                          }/image/upload/v1697492964/${item}`
+                        )
+                      }
+                    >
+                      <img
+                        style={{ width: "100%" }}
+                        src={`https://res.cloudinary.com/${
+                          import.meta.env.VITE_CLOUD_NAME
+                        }/image/upload/v1697492964/${item}`}
+                      />
+                    </div>
+                  ))
+                : null}
+              {selectedItem && selectedItem.imgs.length < 3 && editmode && (
+                <StyledUpload
+                  style={{ width: "50px" }}
+                  listType="picture-card"
+                  multiple
+                  className="upload-list-inline"
+                  {...props1}
+                >
+                  <div>
+                    <PlusOutlined />
+                  </div>
+                </StyledUpload>
+              )}
+            </DetailImgDetail>
+          </DetailImgContainer>
 
-      <EditPanel>
-        <Input
-          defaultValue={selectedItem?.name}
-          addonBefore="Nombre"
-          className="input__addform precio"
-          placeholder="Buscar por nombre"
-          type="text"
-          disabled={!editmode}
-          onChange={(value) => handleChange(value.target.value, "name")}
-        />
-        <Input
-          defaultValue={selectedItem?.price}
-          addonBefore="Precio"
-          className="input__addform precio"
-          placeholder="Buscar por nombre"
-          type="number"
-          disabled={!editmode}
-          onChange={(value) => handleChange(value.target.value, "price")}
-        />
-        <Select
-          defaultValue={selectedItem?.brand}
-          onChange={(value) => handleChange(value, "brand")}
-          className="select__mproducts"
-          options={[
-            { label: "Elige una marca", value: "" },
-            { label: "Adidas", value: "ADIDAS" },
-            { label: "Nike", value: "NIKE" },
-            { label: "New Balance", value: "NEW BALANCE" },
-            { label: "Air Jordan", value: "AIR JORDAN" },
-            { label: "Yeezy", value: "YEEZY" },
-            { label: "Converse", value: "CONVERSE" },
-            { label: "Vans", value: "VANS" },
-            { label: "Revengexstorm", value: "REVENGEXSTORM" },
-          ]}
-          disabled={!editmode}
-        />
-        <Select
-          defaultValue={selectedItem?.genre}
-          onChange={(value) => handleChange(value, "genre")}
-          className="select__mproducts"
-          options={[
-            { value: "", label: "Elige un genero" },
-            { value: "MEN", label: "Hombre" },
-            { value: "WOMAN", label: "Mujer" },
-            { value: "UNISEX", label: "Unisex" },
-          ]}
-          disabled={!editmode}
-        />
-        <div style={{ display: "flex", gap: "5px", margin: "10px 0px" }}>
-          <StyledCustomButton
-            onClick={() => handleUpdateProduct(selectedItem)}
-            disabled={!editmode}
-            title="Guardar edicion"
-          >
-            Editar
-          </StyledCustomButton>
-          <StyledCustomButton
-            style={{ fontSize: "12px" }}
-            onClick={() => handleDeleteProduct(selectedItem._id)}
-            disabled={!editmode}
-            title="Eliminar producto"
-          >
-            Eliminar
-          </StyledCustomButton>
-          {/* <StyledCustomButton
+          <EditPanel>
+            <Input
+              defaultValue={selectedItem?.name}
+              addonBefore="Nombre"
+              className="input__addform precio"
+              placeholder="Buscar por nombre"
+              type="text"
+              disabled={!editmode}
+              onChange={(value) => handleChange(value.target.value, "name")}
+            />
+            <Input
+              defaultValue={selectedItem?.price}
+              addonBefore="Precio"
+              className="input__addform precio"
+              placeholder="Buscar por nombre"
+              type="number"
+              disabled={!editmode}
+              onChange={(value) => handleChange(value.target.value, "price")}
+            />
+            <Select
+              defaultValue={selectedItem?.brand}
+              onChange={(value) => handleChange(value, "brand")}
+              className="select__mproducts"
+              options={[
+                { label: "Elige una marca", value: "" },
+                { label: "Adidas", value: "ADIDAS" },
+                { label: "Nike", value: "NIKE" },
+                { label: "New Balance", value: "NEW BALANCE" },
+                { label: "Air Jordan", value: "AIR JORDAN" },
+                { label: "Yeezy", value: "YEEZY" },
+                { label: "Converse", value: "CONVERSE" },
+                { label: "Vans", value: "VANS" },
+                { label: "Revengexstorm", value: "REVENGEXSTORM" },
+              ]}
+              disabled={!editmode}
+            />
+            <Select
+              defaultValue={selectedItem?.genre}
+              onChange={(value) => handleChange(value, "genre")}
+              className="select__mproducts"
+              options={[
+                { value: "", label: "Elige un genero" },
+                { value: "MEN", label: "Hombre" },
+                { value: "WOMAN", label: "Mujer" },
+                { value: "UNISEX", label: "Unisex" },
+              ]}
+              disabled={!editmode}
+            />
+            <div style={{ display: "flex", gap: "5px", margin: "10px 0px" }}>
+              <StyledCustomButton
+                onClick={() => handleUpdateProduct(selectedItem)}
+                disabled={!editmode}
+                title="Guardar edicion"
+              >
+                Editar
+              </StyledCustomButton>
+              <StyledCustomButton
+                style={{ fontSize: "12px" }}
+                onClick={() => handleDeleteProduct(selectedItem._id)}
+                disabled={!editmode}
+                title="Eliminar producto"
+              >
+                Eliminar
+              </StyledCustomButton>
+              {/* <StyledCustomButton
             onClick={() => console.log(selectedItem)}
             disabled={!editmode}
             title="Agregar imagenes"
           >
             Agregar imagenes
           </StyledCustomButton> */}
-        </div>
-      </EditPanel>
+            </div>
+          </EditPanel>
 
-      <StockContainer>
-        <h3 style={{ marginBottom: "10px" }}>Stock</h3>
-        <div className="badge__container">
-          <Space size="middle">
-            {selectedItem &&
-              selectedItem.sizes &&
-              selectedItem.sizes.length > 0 &&
-              selectedItem.sizes.map((item: any) => (
-                <div className="button_badge">
-                  <button
-                    className="button__delete_badge"
-                    // onClick={() => handleDeleteStock(index)}
-                    title="Eliminar producto del stock"
-                  >
-                    x
-                  </button>
-                  <Badge size="small" count={item.qty} color={"#4E7A9C"}>
-                    <Avatar shape="square" size="small">
-                      {item.size}
-                    </Avatar>
-                  </Badge>
-                </div>
-              ))}
-          </Space>
+          <StockContainer>
+            <h3 style={{ marginBottom: "10px" }}>Stock</h3>
+            <div className="badge__container">
+              <Space size="middle">
+                {selectedItem &&
+                  selectedItem.sizes &&
+                  selectedItem.sizes.length > 0 &&
+                  selectedItem.sizes.map((item: any) => (
+                    <div className="button_badge">
+                      <button
+                        className="button__delete_badge"
+                        // onClick={() => handleDeleteStock(index)}
+                        title="Eliminar producto del stock"
+                      >
+                        x
+                      </button>
+                      <Badge size="small" count={item.qty} color={"#4E7A9C"}>
+                        <Avatar shape="square" size="small">
+                          {item.size}
+                        </Avatar>
+                      </Badge>
+                    </div>
+                  ))}
+              </Space>
+            </div>
+          </StockContainer>
         </div>
-      </StockContainer>
+      )}
     </div>
   );
 };
